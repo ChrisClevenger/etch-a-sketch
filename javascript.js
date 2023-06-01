589f0769fc471d32419bdb2bbf892334c54eeb6c
ChrisClevenger <aenguscoen@gmail.com>
2023-06-01 09:03:43 -0500

    Edit js/css to resize sketchContainer

--- javascript.js
+++ javascript.js
@@ -3,10 +3,10 @@ function buildPixels(value) {
 
     for (x = 0; x < (value); x++) {
         const pixelBox = document.createElement('div'); 
-        pixelBox.style.cssText = "background-color: blue; height: 32px; width: 32px; margin: 0; display: flex; flex: 1, 0; flex-wrap: wrap; align-content: flex-start; border-style: solid; padding: 0;"
+        pixelBox.style.cssText = "background-color: blue; border-style: solid;"
         sketchContainer.appendChild(pixelBox); 
         console.log(value); 
     }
 }
 
-buildPixels(256); 
+buildPixels(1024); 
